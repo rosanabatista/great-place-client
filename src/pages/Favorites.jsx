932f7b0d82e0a7e9e6f4f3c48aea8ae17230d9a2
Rosana Batista <rosana.psb@gmail.com>
2020-12-14 22:59:13 +0100

    favorites and redo the search

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,4 @@
-import { get } from "../services/api";
+import { get, post } from "../services/api";
 import React, { Component } from "react";
 import Place from "../components/Place/Place";
 
@@ -12,12 +12,27 @@ export default class Favorites extends Component {
       this.setState({ places: result.data });
     });
   };
+  handleFavoriteClick = (event) => {
+    event.preventDefault();
+    post(`/favorites/${event.target.dataset.id}`).then((result) => {
+      const favorites = this.state.places;
+      const newFavorites = favorites.filter(
+        (favorite) => favorite.place_id !== event.target.dataset.id
+      );
+      this.setState({ places: newFavorites });
+    });
+  };
 
   render() {
     return (
       <div>
         {this.state.places.map((place) => {
-          return <Place place={place} />;
+          return (
+            <Place
+              place={place}
+              handleFavoriteClick={this.handleFavoriteClick}
+            />
+          );
         })}
       </div>
     );
